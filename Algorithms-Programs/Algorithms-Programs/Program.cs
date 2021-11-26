76235d105b1466fdf69c55c5d03a88805dec740e
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-26 20:01:36 +0530

    [Gyanendra] Add: A Program of Binary Search

--- Algorithms-Programs/Algorithms-Programs/Program.cs
+++ Algorithms-Programs/Algorithms-Programs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Algorithms_Programs
 {
@@ -9,7 +11,7 @@ namespace Algorithms_Programs
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Permutation Program, 2-Exit");
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Permutation Program, 2-Binary Search,3-Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -19,7 +21,14 @@ namespace Algorithms_Programs
                         int n = str.Length;
                         Permutation.Permutate(str, 0, n - 1);
                         break;
-                    case 2: flag = false;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"E:\GitDemo\Algorithms-Problems\Algorithms-Programs\Algorithms-Programs\Utility\WordFile.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    case 3:
+                        flag = false;
                         break;
                 }
             }
