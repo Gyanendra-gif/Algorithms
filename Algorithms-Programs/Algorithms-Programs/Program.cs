93cf29897a655aa8226824f5f13364418ee9c705
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-26 19:47:28 +0530

    [Gyanendra] Add: A Program of Permutation

---
+++ Algorithms-Programs/Algorithms-Programs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithms_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Permutation Program, 2-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to Find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutation.Permutate(str, 0, n - 1);
+                        break;
+                    case 2: flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
